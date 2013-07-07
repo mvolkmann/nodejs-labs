fb0a35c1cb652859f8509e515c4743d6bb890b41
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2013-07-07 11:47:13 -0500

    fixed require of stdio-rpn

--- rpn/file-rpn.js
+++ rpn/file-rpn.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var evaluate = require('./stdio-rpn');
+var rpn = require('./stdio-rpn');
 
 var filePath = process.argv[2];
 if (!filePath) {
@@ -15,7 +15,7 @@ var liner = new Liner(filePath);
 var result;
 
 liner.on('data', function (line) {
-  result = evaluate(line, true);
+  result = rpn.evaluate(line, true);
 });
 
 liner.on('end', function () {
